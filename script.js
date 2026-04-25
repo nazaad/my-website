75ee625f5aa35fd4c3142c09e49d098dd9693b73
nazaad <inamnazaad@gmail.com>
2026-04-25 14:45:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 let articles = [];
 
 fetch("articles.json")
-.then(res => res.json())
-.then(data => {
-  articles = data;
-  show(data);
-});
+  .then(res => res.json())
+  .then(data => {
+    articles = data;
+    render(data);
+  });
 
-function show(data){
+function render(data){
   let list = document.getElementById("list");
   list.innerHTML = "";
 
   data.forEach((a, i) => {
     list.innerHTML += `
       <div class="card">
-        <h3>${a.title}</h3>
-        <p>${a.content.substring(0,100)}...</p>
-        <a href="article.html?id=${i}">Read More</a>
+        <h2>${a.title}</h2>
+        <p>${a.content.substring(0,140)}...</p>
+        <a href="article.html?id=${i}">Read full article</a>
       </div>
     `;
   });
 }
 
-document.getElementById("search").addEventListener("input", e => {
-  let val = e.target.value.toLowerCase();
-  show(articles.filter(a => a.title.toLowerCase().includes(val)));
+// SMART SEARCH
+document.addEventListener("input", (e) => {
+  if(e.target.id === "search"){
+    let val = e.target.value.toLowerCase();
+
+    let filtered = articles.filter(a =>
+      a.title.toLowerCase().includes(val) ||
+      a.content.toLowerCase().includes(val)
+    );
+
+    render(filtered);
+  }
 });
